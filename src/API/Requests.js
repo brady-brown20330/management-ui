4e884e06a7417fe566d301b734f6d0b4256cd4eb
Brady <brown@Bradys-MacBook-Pro.local>
2021-09-27 18:12:29 -0400

    completes initial setup

---
+++ src/API/Requests.js
@@ -0,0 +1,8 @@
+const contentstack = require("contentstack")
+
+const Stack = contentstack.Stack({
+  api_key: process.env.REACT_APP_APIKEY,
+  delivery_token: process.env.REACT_APP_DELIVERY_TOKEN,
+  environment: process.env.REACT_APP_ENVIRONMENT,
+  region: process.env.REACT_APP_REGION ? process.env.REACT_APP_REGION : "us",
+})
