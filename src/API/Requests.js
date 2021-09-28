26872f07f70ad6b4f3ed914578a75b50fa6431f0
Brady <brown@Bradys-MacBook-Pro.local>
2021-09-28 11:40:46 -0400

    completed basic configuration for node SDK

--- src/API/Requests.js
+++ src/API/Requests.js
@@ -1,16 +1,39 @@
-const contentstack = require("contentstack")
+const managementInfo = require('./managementInfo.js')
 
-const Stack = contentstack.Stack({
-  api_key: process.env.REACT_APP_APIKEY,
-  delivery_token: process.env.REACT_APP_DELIVERY_TOKEN,
-  environment: process.env.REACT_APP_ENVIRONMENT,
-  region: process.env.REACT_APP_REGION ? process.env.REACT_APP_REGION : "us",
+const contentstack = require('@contentstack/management')
+const contentstackClient = contentstack.client({ authtoken: managementInfo.REACT_APP_MANAGEMENT_TOKEN }) 
+
+// clogin to contentstack
+contentstackClient.login({ email: 'brady.brown@contentstack.com', password: managementInfo.CONTENTSTACK_PASSWORD})
+.then((response) => {
+    console.log(response.notice)
+    console.log(response.user)
+}) 
+
+// create an entry
+// var entry  = {
+//   title: 'Sample Entry',
+//   url: '/sampleEntry'
+// }
+
+contentstackClient.stack({ api_key: managementInfo.REACT_APP_APIKEY }).contentType('projects').entry().create({ entry })
+.then((entry) => {
+  console.log(entry)
 })
 
-// create an entry 
+export default {
+  // should be able to call from managementForm.jsx
+  createEntry(api_key, entry){
+    return new Promise((resolve, reject) => {
+      contentstackClient.stack({ api_key: api_key })
+      .contentType('projects')
+      .entry()
+      .create({ entry })
+      .then((entry) => {
+        console.log(entry)
+      })
+    })
+  }
+    
 
-// publish an entry
-
-// add an asset
-
-// export all functions
+}
